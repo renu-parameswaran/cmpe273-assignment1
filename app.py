44583a9a23b92714d212b78573014436765a2d01
renu-parameswaran <renu.parameswaran@gmail.com>
2017-03-17 14:27:35 -0700

    changed the loop

--- app.py
+++ app.py
@@ -2,20 +2,17 @@ from github import Github
 import sys,os
 from flask import Flask
 
-
-
 app = Flask(__name__)
 
 @app.route("/v1/<filename>")
 def displayfilecontent(filename):
  cli = (sys.argv[1]).split("/")
- config_file=filename
  repo_name=cli[4]
  username=cli[3]
  repo=Github().get_user(username).get_repo(repo_name)
  extensions = '.yml','.json'
  output=[]
- if (filename == config_file):
+ if (filename != None):
   if filename.endswith(extensions):
    f=repo.get_contents(filename)
    output.append(f.decoded_content)
